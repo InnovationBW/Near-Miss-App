661d6a1c8e3bddf47f350cf228ff5c77c46d4c67
tuteredurie <tuteredurie@hotmail.com>
2023-10-12 14:36:20 +1300

    test db changes

--- connectionInfo.php
+++ connectionInfo.php
@@ -1,7 +1,7 @@
 <?php
     //Configuration of mysql database for backend processing
-	$mysql_host="cmslamp14";
-	$mysql_user="nearmiss";
-	$mysql_pass="cHz4n3armiss2022";
-	$mysql_db="nearmiss";
+	$mysql_host="aws.connect.psdb.cloud";
+	$mysql_user="i7lp34ltoue5qohb1rcu";
+	$mysql_pass="pscale_pw_u4tNWH9Xd4GOc7MDTfLKr8QNLGoVmyX65POP10mNXyp";
+	$mysql_db="near-miss";
 ?>
