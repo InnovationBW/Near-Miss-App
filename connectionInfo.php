fe3bd9c4579e38533f399d9a542490edd355d2d7
tuteredurie <tuteredurie@hotmail.com>
2023-10-17 16:35:03 +1300

    db changed to env

--- connectionInfo.php
+++ connectionInfo.php
@@ -1,8 +1,10 @@
 <?php
+	$_ENV = parse_ini_file('.env');
+
     //Configuration of mysql database for backend processing
-	$mysql_host="aws.connect.psdb.cloud";
-	$mysql_user="b5y9jblai9kfllf7tebc";
-	$mysql_pass="pscale_pw_wXqKW48jcggCNfSNt2uyy2dybR10iyP3FfUSS5vjT47";
-	$mysql_db="near-miss";
-	$mysql_ssl="/etc/ssl/cert.pem";
+	$mysql_host=$_ENV["HOST"];
+	$mysql_user=$_ENV['USERNAME'];
+	$mysql_pass=$_ENV['PASSWORD'];
+	$mysql_db=$_ENV['DATABASE'];
+	$mysql_ssl=$_ENV['MYSQL_ATTR_SSL_CA'];
 ?>
