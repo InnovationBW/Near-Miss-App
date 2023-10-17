e97cce812c3894f724b8d8cab1d1c0f4ab96bf58
tuteredurie <tuteredurie@hotmail.com>
2023-10-17 15:57:42 +1300

    updated connection info

--- connectionInfo.php
+++ connectionInfo.php
@@ -1,8 +1,8 @@
 <?php
     //Configuration of mysql database for backend processing
 	$mysql_host="aws.connect.psdb.cloud";
-	$mysql_user="qebijmksjzglvk6xulwe";
-	$mysql_pass="pscale_pw_7xpaXNZuMiPW7h1MyuUUKVx89ZJ6UGxz3OkipqIH9J";
+	$mysql_user="ihlll0b2b5gm4jwn6yj0";
+	$mysql_pass="pscale_pw_7PvFJPAnu5xtBvXjG6Cj9X8ewV6LPBKDb24Nj1otf1Z";
 	$mysql_db="near-miss";
 	$mysql_ssl="/etc/ssl/cert.pem";
 ?>
